4eb958c95ff95b00a30f0f15482b950a65952bff
Lime Icing <emil577b@stud.kea.dk>
2018-02-15 17:51:14 +0100

    Assignment prepped

---
+++ Main15.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class Main15 {
+
+    /*
+    Write a method maxOccurrences that accepts a list of integers as a parameter and returns the number of times the
+    most frequently occurring integer (the “mode”) occurs in the list. Solve this problem using a map as auxiliary
+    storage. If the list is empty, return 0 .
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxOccurences(List<Integer> list) {
+        if (list.isEmpty()) {
+            return 0;
+        }
+
+        HashMap aux = new HashMap();
+        return -1;
+    }
+}
